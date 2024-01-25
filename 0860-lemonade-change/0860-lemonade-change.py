7293707d20d34433973e21ebfc51e3d20d267a18
mamulee <75348994+mamulee@users.noreply.github.com>
2024-01-25 22:05:09 +0900

    Time: 621 ms (91.14%), Space: 21.2 MB (53.94%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.py
+++ 0860-lemonade-change/0860-lemonade-change.py
@@ -1,22 +1,22 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        salary = {5: 0, 10: 0}
+        change = {5: 0, 10: 0}
 
         for i in bills:
             if i == 5:
-                salary[5] += 1
+                change[5] += 1
             elif i == 10:
-                if salary[5] == 0:
+                if change[5] == 0:
                     return False
-                salary[5] -= 1
-                salary[10] += 1
+                change[5] -= 1
+                change[10] += 1
             elif i == 20:
-                if salary[10] > 0 and salary[5] > 0:
-                    salary[10] -= 1
-                    salary[5] -= 1
-                elif salary[5] >= 3:
-                    salary[5] -= 3
+                if change[10] > 0 and change[5] > 0:
+                    change[10] -= 1
+                    change[5] -= 1
+                elif change[5] >= 3:
+                    change[5] -= 3
                 else:
                     return False
-        
+
         return True
